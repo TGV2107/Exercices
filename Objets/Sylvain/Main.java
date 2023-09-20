67d8e57204227fb0e7bca2a05129c076ae21fee0
History54 <quentinbeyl01@gmail.com>
2023-09-20 08:49:13 +0200

    Tulipe ??

--- Objets/Sylvain/Main.java
+++ Objets/Sylvain/Main.java
@@ -1,5 +1,8 @@
 package Objets.Sylvain;
 
 public class Main {
-    Tulipe Akta = new Tulipe(true, null);
+    public static void main(String[] args) {
+        Tulipe Akta = new Tulipe("Akta", true, null);
+        System.out.println(Akta.getName());
+    }
 }
