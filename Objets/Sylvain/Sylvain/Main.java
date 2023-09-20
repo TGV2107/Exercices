8063ae573d80568c9a1c81cd59fc833781701278
History54 <quentinbeyl01@gmail.com>
2023-09-20 09:11:58 +0200

    Noob

--- Objets/Sylvain/Sylvain/Main.java
+++ Objets/Sylvain/Sylvain/Main.java
@@ -1,7 +1,7 @@
 package Sylvain;
 
 public class Main {
-    public Main(String[] args) {
+    public static void main(String[] args) {
         Tulipe Akta = new Tulipe("Akta", true, null);
         System.out.println(Akta.getName());
     }
