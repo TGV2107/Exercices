4de27b2b0e2796223968897b3c6b937a4b85021c
History54 <quentinbeyl01@gmail.com>
2023-09-20 08:20:10 +0200

    tulipe.java :"

---
+++ Objets/Sylvain/tulipe.java
@@ -0,0 +1,13 @@
+package Objets.Sylvain;
+
+public class tulipe {
+    private boolean isNoob;
+
+    public tulipe(boolean isNoob){
+        this.isNoob = isNoob;
+    }
+
+    public boolean isNoob(){
+        return this.isNoob;
+    }
+}
