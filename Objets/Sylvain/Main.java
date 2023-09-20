1a90d3169de55f18964b81ff8fefd51852ab07e9
TGV2107 <122354290+TGV2107@users.noreply.github.com>
2023-09-20 08:50:59 +0200

    main SANS M

--- Objets/Sylvain/Main.java
+++ Objets/Sylvain/Main.java
@@ -1,6 +1,6 @@
 package Objets.Sylvain;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         Tulipe Akta = new Tulipe("Akta", true, null);
         System.out.println(Akta.getName());
