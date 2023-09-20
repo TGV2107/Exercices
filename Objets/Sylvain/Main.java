9ef850ab37593e870822899c521fe84b6293ca40
History54 <quentinbeyl01@gmail.com>
2023-09-20 08:51:32 +0200

    Majuscule !!!!

--- Objets/Sylvain/Main.java
+++ Objets/Sylvain/Main.java
@@ -1,5 +1,6 @@
 package Objets.Sylvain;
 
+//AVEC UNE MAJUSCULE LE NOM DE LA CLASSE
 public class Main {
     public static void main(String[] args) {
         Tulipe Akta = new Tulipe("Akta", true, null);
