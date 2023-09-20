0c64c85c97c1f90b2df73bc0a0445736f3ed3202
TGV2107 <122354290+TGV2107@users.noreply.github.com>
2023-09-20 08:24:04 +0200

    sylvain is noob

--- Objets/Sylvain/tulipe.java
+++ Objets/Sylvain/tulipe.java
@@ -2,6 +2,7 @@ package Objets.Sylvain;
 
 public class tulipe {
     private boolean isNoob;
+    private Map<int> noob = new Hashmap<>();
 
     public tulipe(boolean isNoob){
         this.isNoob = isNoob;
