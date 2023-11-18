56ee2bf5c45f7c118e7fb1be7059213a5f42aa19
sylvaneaucode <sylvain.craftmine@gmail.com>
2023-11-18 15:00:09 +0100

    save

--- Modularité et gestion des bugs/Sylvain/NSI66_6.py
+++ Modularité et gestion des bugs/Sylvain/NSI66_6.py
@@ -1,5 +1,5 @@
 def bissextile(n:int)->bool:
-    if n%4 == 0 and n%400 == 0 or n%100 != 0:
+    if (n%4 == 0 and n%100!= 0) or (n%400 == 0):
         return True
     return False
 
@@ -7,7 +7,7 @@ def bissextile(n:int)->bool:
 def bis(n:int)-> bool:
     if n%4 == 0:
         if n%100 == 0:
-            if n%400:
+            if n%400 == 0:
                 return True
             return False
         return True
--- Modularité et gestion des bugs/Sylvain/test_NSI66_6.py
+++ Modularité et gestion des bugs/Sylvain/test_NSI66_6.py
@@ -2,7 +2,7 @@ from NSI66_6 import*
 import pytest
 
 def test_bn_bissextile1():
-    assert type(bissextile(n)) == bool,"Il y a un problème de type"
+    assert type(bissextile(100)) == bool,"Il y a un problème de type"
     with pytest.raises(TypeError):
         bissextile("patrik")
 
@@ -11,13 +11,17 @@ def test_bb_bissextile1():
     assert bissextile(100)== False, "100 est accepté alors qu'il ne devrais pas"
     assert bissextile(400) == True, "400 n'est pas accepté"
     assert bissextile(4) == True, "4 n'est pas accepté"
-    
+    assert bissextile(1) == False, "1 ne doit pas être bissextile"
+
 
 def test_bn_bissextile2():
-    assert type(bissextile(n)) == bool,"Il y a un problème de type"
+    assert type(bissextile(100)) == bool,"Il y a un problème de type"
     with pytest.raises(TypeError):
         bissextile("patrik")
 
 def test_bb_bissextile2():
-    pass
+    assert bis(100)== False, "100 est accepté alors qu'il ne devrais pas"
+    assert bis(4) == True, "4 n'est pas accepté"
+    assert bis(1) == False, "1 ne doit pas être bissextile"
+    assert bis(400) == True, "400 n'est pas accepté"
 
